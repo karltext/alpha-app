68a221d68c6136e6f05fcfe0519b4c697efea9ae
ben61088 <44975057+ben61088@users.noreply.github.com>
2019-01-21 14:41:05 +0000

    Classes added:
    Flight
    FlightAccessAPI
    FlightJPARepository

---
+++ alpha/src/main/java/com/mastek/alpha/Flight.java
@@ -0,0 +1,58 @@
+package com.mastek.alpha;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="FLIGHT_ENTITY_TABLE")
+public class Flight {
+	
+	int flightNumber;
+	String departureTime;
+	double length_hours;
+	String company;
+	String origin;
+	String destination;
+	
+	@Id
+	public int getFlightNumber() {
+		return flightNumber;
+	}
+	public void setFlightNumber(int flightNumber) {
+		this.flightNumber = flightNumber;
+	}
+	public String getDepartureTime() {
+		return departureTime;
+	}
+	public void setDepartureTime(String departureTime) {
+		this.departureTime = departureTime;
+	}
+	public double getLength_hours() {
+		return length_hours;
+	}
+	public void setLength_hours(double length_hours) {
+		this.length_hours = length_hours;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	
+	
+
+}
