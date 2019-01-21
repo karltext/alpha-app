e40652847d9a23c41487e684edf0fb5f69676b1a
KHAYAM1808X <44975292+KHAYAM1808X@users.noreply.github.com>
2019-01-21 15:13:37 +0000

    Yo bwois

--- alpha/src/main/java/com/mastek/alpha/Account.java
+++ alpha/src/main/java/com/mastek/alpha/Account.java
@@ -1,13 +1,25 @@
 package com.mastek.alpha;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Account_ENTITY_TABLE")
 public class Account {
 
 	int accountNumber; 
 	String accountType;
 	double bonuspoints;
+	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
 	public int getAccountNumber() {
 		return accountNumber;
 	}
+	
 	public void setAccountNumber(int accountNumber) {
 		this.accountNumber = accountNumber;
 	}
---
+++ alpha/src/main/java/com/mastek/alpha/AccountJPARepository.java
@@ -0,0 +1,7 @@
+package com.mastek.alpha;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountJPARepository extends CrudRepository<Account, Integer> {
+
+}
