1bbfe06149a83c857db76e3c786eccda529a776f
karltext <44974965+karltext@users.noreply.github.com>
2019-01-21 14:06:04 +0000

    initialised project

---
+++ alpha/src/main/java/com/mastek/alpha/AlphaApplication.java
@@ -0,0 +1,15 @@
+package com.mastek.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		SpringApplication.run(AlphaApplication.class, args);
+	}
+
+}
+
