d023aaa4bf1083af64cad05e20d05c3186756929
Oliver <44974906+ocgMastek@users.noreply.github.com>
2019-01-21 15:08:55 +0000

    Hotel API

--- alpha/src/main/java/com/mastek/alpha/AlphaApplication.java
+++ alpha/src/main/java/com/mastek/alpha/AlphaApplication.java
@@ -2,6 +2,7 @@ package com.mastek.alpha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AlphaApplication {
@@ -9,6 +10,7 @@ public class AlphaApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		SpringApplication.run(AlphaApplication.class, args);
+		
 	}
 
 }
---
+++ alpha/src/main/java/com/mastek/alpha/ServiceConfig.java
@@ -0,0 +1,17 @@
+package com.mastek.alpha;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class ServiceConfig extends ResourceConfig {
+	
+	public ServiceConfig() {
+		register(BookingAccessAPI.class);
+		register(TrainAccessAPI.class);
+//		register(FlightAccessAPI.class);
+//		register(AccountAccessAPI.class);
+//		register(HotelAccessAPI.class);
+
+//		register(CustomerAccessAPI.class);
+	}
+
+}
