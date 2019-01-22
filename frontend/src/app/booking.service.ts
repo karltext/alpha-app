4e391182b5033e3faf4b23b674871ce500793b17
karltext <44974965+karltext@users.noreply.github.com>
2019-01-22 14:36:32 +0000

    sup

--- frontend/src/app/booking.service.ts
+++ frontend/src/app/booking.service.ts
@@ -10,25 +10,18 @@ export class BookingService {
 
   constructor(private http: HttpClient) { }
 
-  rootURL = "http://localhost:9900/bookings/"
+  rootURL = "http://localhost:9900/bookings"
 
   listBookings(): Observable<Booking[]> {
-    return this.http.get<Booking[]>(this.rootURL + "list")
+    return this.http.get<Booking[]>(this.rootURL + "/list")
   }
 
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
+  registerBooking(bookingType: string) {
+    const http0pts = {
+      headers: new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'})
     }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
-  };
+    const reqBody = "bookingType=" + bookingType
+    const url = this.rootURL + "/register"
+    return this.http.post<Booking>(url, reqBody, http0pts)
+  }
 }
