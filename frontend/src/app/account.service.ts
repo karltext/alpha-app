74ca58e3e289494c2b17fde61fae07723abbee1e
lukerowley101 <44974779+lukerowley101@users.noreply.github.com>
2019-01-22 14:57:37 +0000

    account

--- frontend/src/app/account.service.ts
+++ frontend/src/app/account.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '../../node_modules/@angular/common/http';
+import { Observable } from '../../node_modules/rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+rootURL:string
 
-  constructor() { }
+  constructor(private httpsvc:HttpClient) {
+    this.rootURL="http://localhost:9000/accounts"
+   }
+   getAccounts():Observable<Account[]>{
+     return this.httpsvc.get<Account[]>(this.rootURL+"/list")
+   }
+   
+addNewAccount(newAccount:Account):Observable<Account>{
+  const httpOpts ={
+    headers: new HttpHeaders(
+      {'Content-Type':
+      'application/x-www-form-urlencoded;charset=UTF-8'})
+      }
+      var reqBody="accountType="+newAccount.accountType+"&bonuspoints="+newAccount.bonuspoints
+
+      return this.httpsvc.post<Account>(
+        this.rootURL+"/register",reqBody,httpOpts)
 }
+deleteAccount(accountNumber:number):Observable<Account>{
+  return this.httpsvc.request<Account>('DELETE',this.rootURL+"/delete",
+{
+  headers:new HttpHeaders({'Content-Type': 'text/plain'}),
+  body:accountNumber
+})
+}
+
+  }
+
